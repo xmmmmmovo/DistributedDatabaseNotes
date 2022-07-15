e89577527820e4b49cb6d1158e6d94c2daa0482f
xmmmmmovo <13256172360@163.com>
2022-07-15 19:05:38 +0800

    db

--- MIT6.824Lab/src/mr/worker.go
+++ MIT6.824Lab/src/mr/worker.go
@@ -11,7 +11,7 @@ import "log"
 import "net/rpc"
 import "hash/fnv"
 
-//
+// KeyValue
 // Map functions return a slice of KeyValue.
 //
 type KeyValue struct {
@@ -21,7 +21,7 @@ type KeyValue struct {
 
 type ByKey []KeyValue
 
-// for sorting by key.
+// Len for sorting by key.
 func (a ByKey) Len() int           { return len(a) }
 func (a ByKey) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
 func (a ByKey) Less(i, j int) bool { return a[i].Key < a[j].Key }
